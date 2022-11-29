a38a5868f13f9ad3c35bb1441befb3b625fa9192
Daniel Trang <danieltrangnotes@gmail.com>
2022-11-29 11:06:49 -0800

    fixed import path

--- ecomapper-app/src/routes.jsx
+++ ecomapper-app/src/routes.jsx
@@ -1,17 +1,16 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import './App.css'
 
 function EcomapperRoutes() {
 
     return (
         <BrowserRouter>
             <Routes>
+                {/* this is left as an exercise for you to learn how to hook up pages */}
                 <Route index element={/*landing page here */ }/>
                 <Route path="" element={/*map tool page here*/ }/>
             </Routes>
         </BrowserRouter>
     )
-    
 }
 
 export default EcomapperRoutes
