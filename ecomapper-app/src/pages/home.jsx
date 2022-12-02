53dcf4d0f89cc41cadad559079bffcde20c94895
Thomas <trg5@pdx.edu>
2022-12-01 17:50:38 -0800

    Began working on landing page

---
+++ ecomapper-app/src/pages/home.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <>
+      <div class="flex justify-center">
+        <h1>Hello World!</h1>
+        <button class="btn-ecomapper">Button</button>
+      </div>
+    </>
+  );
+};
+
+export default Home;
--- ecomapper-app/src/routes.jsx
+++ ecomapper-app/src/routes.jsx
@@ -1,16 +1,14 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home.jsx';
 
-function EcomapperRoutes() {
-
-    return (
-        <BrowserRouter>
-            <Routes>
-                {/* this is left as an exercise for you to learn how to hook up pages */}
-                <Route index element={/*landing page here */ }/>
-                <Route path="" element={/*map tool page here*/ }/>
-            </Routes>
-        </BrowserRouter>
-    )
+export default function EcomapperRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* this is left as an exercise for you to learn how to hook up pages */}
+        <Route index element={<Home />} />
+        {/* <Route path="" element={ map tool page here}/> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default EcomapperRoutes
