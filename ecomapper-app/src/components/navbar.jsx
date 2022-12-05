a57b682ef6d2db43f9b704de8ff3df36622b2537
Daniel Trang <danieltrangnotes@gmail.com>
2022-12-04 23:44:41 -0800

    cleaning up landing page styling

--- ecomapper-app/src/components/navbar.jsx
+++ ecomapper-app/src/components/navbar.jsx
@@ -1,15 +1,15 @@
-import logo_image from '../assets/t_logo.png'
+import logo_image from '../assets/ecomap_transparent_logo.png'
 import profile_pic from '../assets/chubby_yellow_duck.png'
 import { Link } from 'react-router-dom'
 
 function NavbarProp() {
     return (
         <>
-            <nav class="flex items-center px-4 py-2 bg-blue-900/75 shadow-2xl">
+            <nav class="flex items-center sticky top-0 z-50 px-4 py-2 bg-slate-500/75 shadow-2xl">
                 <Link to="/">
                     <div class="cursor-pointer mx-2 flex items-center flex-shrink-0">
-                        <img class="mr-4 h-8 w-8" src={logo_image} alt="" />
-                        <div class="font-mono text-xl font-semibold tracking-wider text-white">Tredagle</div>
+                        <img class="mr-4 h-8 w-8" src={logo_image} alt="Home Page" />
+                        <div class="font-mono text-xl font-semibold tracking-wider text-white">Ecomapper</div>
                     </div>
                 </Link>
                 <ul class="hidden md:flex flex-row flex-1 justify-end items-center gap-8 uppercase text-xs">
