ac43d1e6c3f57c1ca29867d6457c4d556748d35f
Timofey Shichalin <shichatim@gmail.com>
2023-01-14 00:24:47 -0800

    search bar (not finished)

--- blurp-app/src/components/searchbar.jsx
+++ blurp-app/src/components/searchbar.jsx
@@ -3,8 +3,15 @@
 function SearchBar() {
     return (
         <>
-            <input type="search">
-            </input>
+            <form class="absolute w-full flex items-center justify-center m-1">   
+            <div class="relative w-3/5">
+                <div class="absolute inset-y-0 left-[10px] flex items-center pl- pointer-events-none">
+                    <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                </div>
+                <input type="text" id="voice-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-l-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search map" required />
+            </div>
+            <button type="submit" class="inline-flex items-center py-2.5 px-3 ml-0 text-sm font-medium text-white bg-green-600 rounded-r-lg border border-green-600 hover:bg-green-700 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Search</button>
+        </form>
         </>
     );
 }
