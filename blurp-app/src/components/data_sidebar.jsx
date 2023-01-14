d7b428b726e5917392a83824bdd4c70966f158bf
Timofey Shichalin <shichatim@gmail.com>
2023-01-14 02:38:24 -0800

    data sidebar jsx file

---
+++ blurp-app/src/components/data_sidebar.jsx
@@ -0,0 +1,13 @@
+
+
+function DataSidebar() {
+    return (
+        <>
+            {/* <div class="absolute h-full bg-black w-5 right-2 p-0 m-0">
+                
+            </div> */}
+        </>
+    );
+}
+
+export default DataSidebar;
