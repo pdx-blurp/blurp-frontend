ea23a7720ac5503cfc7f81198a6021692dae1a9d
Daniel Trang <danieltrangnotes@gmail.com>
2023-01-19 21:25:45 -0800

    merged test-page into blurpmap.jsx

--- blurp-app/src/routes.jsx
+++ blurp-app/src/routes.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home.jsx';
 import BlurpMap from './pages/blurpmap.jsx';
-import TestPage from './pages/testpage.jsx';
 
 export default function BlurpRoutes() {
   return (
@@ -10,7 +9,6 @@ export default function BlurpRoutes() {
         {/* this is left as an exercise for you to learn how to hook up pages */}
         <Route index element={<Home />} />
         <Route path="/blurpmap" element={<BlurpMap />} />
-        <Route path="/test-page" element={<TestPage />} />
         {/* <Route path="" element={ map tool page here}/> */}
       </Routes>
     </BrowserRouter>
