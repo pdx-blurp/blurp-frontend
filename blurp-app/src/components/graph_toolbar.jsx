40e1066e68ef71906e3534cc2e01b7b54bd7616f
Timofey Shichalin <shichatim@gmail.com>
2023-01-24 03:12:53 -0800

    centered buttons, fixed borders

--- blurp-app/src/components/graph_toolbar.jsx
+++ blurp-app/src/components/graph_toolbar.jsx
@@ -2,14 +2,23 @@ import node_image from "../assets/node_image.svg";
 import edge_image from "../assets/edge_image.svg";
 
 function GraphToolbar () {
+
+  function handleNodeSelection() {
+    alert("You selected 'node'.");
+  }
+
+  function handleEdgeSelection() {
+    alert("You selected 'edge'.");
+  }
+
   return (
     <>
-      <div className="h-full w-[40px] bg-gray-400 h-[420px] my-[90px] right-0 p-0 rounded-l-lg">
-        <button>
-          <img className="w-[35px] h-[35px]" src={node_image}></img>
+      <div className="h-full w-[40px] bg-gray-300 border-[2px] border-gray-400 h-[420px] my-[90px] right-0 p-0 rounded-l-lg justify-center">
+        <button onClick={handleNodeSelection}>
+          <img className="w-[40px] h-[40px]" src={node_image}></img>
         </button>
-        <button>
-          <img className="w-[35px] h-[35px]" src={edge_image}></img>
+        <button onClick={handleEdgeSelection}>
+          <img className="w-[40px] h-[40px]" src={edge_image}></img>
         </button>
       </div>
     </>
