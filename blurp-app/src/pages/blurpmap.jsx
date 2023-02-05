0c4b7e81f83862c567d4138620485743cddaad80
iliaspdx <ilias@pdx.edu>
2023-02-04 21:34:59 -0800

    Insert into the try catch block

--- blurp-app/src/pages/blurpmap.jsx
+++ blurp-app/src/pages/blurpmap.jsx
@@ -40,18 +40,18 @@ const TestPage = () => {
       graph.setNodeAttribute(id, 'label', name);
       graph.setNodeAttribute(id, 'years', years);
       graph.setNodeAttribute(id, 'notes', notes);
+      setNodes((prevNodes) =>
+        prevNodes.map((node) => {
+          if (node.id === id) {
+            return { ...node, label: name };
+          }
+          return node;
+        })
+      );
     } catch {
       console.log('ERROR: failed to retrieve node with that ID');
       console.log('ID used: ' + id);
     }
-    setNodes((prevNodes) =>
-      prevNodes.map((node) => {
-        if (node.id === id) {
-          return { ...node, label: name };
-        }
-        return node;
-      })
-    );
   }
 
   function handleIsNode(data) {
