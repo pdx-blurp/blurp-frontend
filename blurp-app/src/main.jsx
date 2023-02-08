7d3d4a5dc236dbb7996ccfe31d72ffdd442d3b99
Timofey Shichalin <shichatim@gmail.com>
2023-02-08 00:27:35 -0800

    sign-in button, OAuth. Imports not included yet

--- blurp-app/src/main.jsx
+++ blurp-app/src/main.jsx
@@ -1,10 +1,13 @@
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import BlurpRoutes from './routes.jsx'
 import './styles/tailwind.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    < BlurpRoutes/>
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId='220935592619-e7j93usk2h7vhcuoauos59rhgvqlcmsa.apps.googleusercontent.com'>
+    <React.StrictMode>
+      < BlurpRoutes/>
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
