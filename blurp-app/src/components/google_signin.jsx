037e033f0d5a4010d8655e62e0663f862fa8c1a6
Timofey Shichalin <shichatim@gmail.com>
2023-02-10 16:12:41 -0800

    uses non-deprecated dependencies

--- blurp-app/src/components/google_signin.jsx
+++ blurp-app/src/components/google_signin.jsx
@@ -1,35 +1,52 @@
-import React, { useState, useRef } from 'react';
-import { GoogleLogin } from 'react-google-login';
-import { GoogleLogout } from 'react-google-login';
+import React, { useState, useEffect } from 'react';
+import { useGoogleLogin, googleLogout } from '@react-oauth/google';
 import guestPic from '../assets/guest_profile_pic.svg';
 import x_button from '../assets/x_button.svg';
-
-// IMPORTANT!!!
-// The gapi import below is needed even though VSCode shows it
-// as useless. Google OAuth doesn't work without it.
-import { gapi } from 'gapi-script';
-// DO NOT REMOVE THE ABOVE IMPORT!
+import axios from 'axios';
 
 function GoogleLoginButton (props) {
 
   // What to render in place of the sign-in button
   const [renderedContent, setRenderedContent] = useState(signInButton());
-  let current_user = null;
+  const [user, setUser] = useState(null);
+  const [profile, setProfile] = useState(null);
 
-  const onLoginSuccess = (googleUser) => {
-    console.log(googleUser);
-    // Set content to be logout button
-    current_user = googleUser;
-    setRenderedContent(userProfile());
+  // If the user changes, update the profile
+  useEffect(() => {
+    if(user) {
+      axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+        headers: {
+          Authorization: `Basic ${user.access_token}`,
+          Accept: 'application/json'
+        }
+      }).then((res) => {
+        setProfile(res.data);
+      }).catch((err) => console.log(err));
+    }
+    else {
+      setProfile(null);
+    }
+  }, [user]);
+
+  // If the profile changes (logged in, logged out, image change, etc)
+  useEffect(() => {
+    if(profile)
+      setRenderedContent(userProfile(false));
+    else
+      setRenderedContent(signInButton);
+  }, [profile]);
+
+
+  function onLoginSuccess(googleUser) {
+    setUser(googleUser);
   }
   
   const onLoginFailure = (error) => {
-    alert("Google sign-in won't work. Enable third-party cookies in your browser.");
+    alert("Google sign-in failed.");
   }
 
-  const onLogoutSuccess = (googleUser) => {
+  function onLogoutSuccess() {
     setRenderedContent(signInButton());
-    console.log(googleUser);
   }
 
   function closePopout () {
@@ -38,21 +55,23 @@ function GoogleLoginButton (props) {
 
   function expandPopout () {
     setRenderedContent(userProfile(true));
-    console.log('expandPopout');
+  }
+
+  const login = useGoogleLogin({
+    onSuccess: codeResponse => onLoginSuccess(codeResponse),
+    onError: error => onLoginFailure(error)
+  });
+
+
+  function logout () {
+    googleLogout();
+    setUser(null);
   }
   
   function signInButton () {
     return (
       <>
-        <GoogleLogin onSuccess={(googleUser) => {onLoginSuccess(googleUser)}}
-            onFailure={(error) => {onLoginFailure(error);}}
-            render={renderProps => (
-              <li onClick={renderProps.onClick} className='btn-navbar sign-in-btn'>Sign In</li>
-              )}
-              responseType='user'
-              cookiePolicy='single_host_origin'
-            >
-          </GoogleLogin>
+        <li onClick={() => login()} className='btn-navbar sign-in-btn'>Sign In</li>
       </>
     );
   }
@@ -64,30 +83,23 @@ function GoogleLoginButton (props) {
       popoutVisibility = '';
 
     let imageUrl = guestPic;
-    if(current_user && current_user.profileObj && current_user.profileObj.imageUrl)
-      imageUrl = current_user.profileObj.imageUrl;
+    if(profile && profile.picture)
+      imageUrl = profile.picture;
 
     return (
       <>
         <li className="cursor-pointer">
           <img className="w-9 h-9 rounded-full" src={imageUrl} onClick={expandPopout}/>
         </li>
-        {/* <div className='w-[150px] h-[40px] bg-gray-200'> */}
-        <GoogleLogout onLogoutSuccess={onLogoutSuccess}
-            render={renderProps => (
-              <>
-                <div className={'absolute flex items-center mt-[120px] w-[180px] h-[60px] bg-gray-900/75 ' + popoutVisibility}>
-                  <div className={'btn-navbar sign-in-btn inline-block align-middle'} onClick={renderProps.onClick}>
-                    Sign out
-                  </div>
-                  <div>
-                    <img src={x_button} className='cursor-pointer w-9 h-9 rounded-full hover:border-2 hover:border-blue-300/10 hover:bg-blue-300/25' onClick={closePopout}></img>
-                  </div>
-                </div>
-              </>
-            )}
-            />
-        </>
+        <div className={'absolute flex items-center mt-[120px] w-[180px] h-[60px] bg-gray-900/75 ' + popoutVisibility}>
+          <div className={'btn-navbar sign-in-btn inline-block align-middle'} onClick={logout}>
+            Sign out
+          </div>
+          <div>
+            <img src={x_button} className='cursor-pointer w-9 h-9 rounded-full hover:border-2 hover:border-blue-300/10 hover:bg-blue-300/25' onClick={closePopout}></img>
+          </div>
+        </div>
+      </>
     );
   }
     
