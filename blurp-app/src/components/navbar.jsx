c1735f5f48966f422563f755e0a1dc87bf884e86
Thomas <trg5@pdx.edu>
2023-03-03 13:37:56 -0800

    Built a new contact page with a form, an image of PSU, and some links to github and blurp mgmt email

--- blurp-app/src/components/navbar.jsx
+++ blurp-app/src/components/navbar.jsx
@@ -1,19 +1,19 @@
 import logo_image from '../assets/blurp_purplebrain_transparent.png';
-import navbar_logo_text from '../assets/blurp_text_white.png'
+import navbar_logo_text from '../assets/blurp_text_white.png';
 import { Link } from 'react-router-dom';
 import GoogleLoginButton from './google_signin';
 
 function NavbarProp() {
   return (
     <>
-      <nav className="flex items-center sticky top-0 z-50 px-4 py-2 bg-gray-900/75 shadow-2xl">
+      <nav className="sticky top-0 z-50 flex items-center bg-gray-900/75 px-4 py-2 shadow-2xl">
         <Link to="/">
-          <div className="cursor-pointer mx-2 flex items-center flex-shrink-0">
+          <div className="mx-2 flex flex-shrink-0 cursor-pointer items-center">
             <img className="mr-2 h-[23px] w-[28px]" src={logo_image} alt="Home Page" />
-            <img className="h-[25px] w-[64px]" src={navbar_logo_text}/>
+            <img className="h-[25px] w-[64px]" src={navbar_logo_text} alt="blurp logo text" />
           </div>
         </Link>
-        <ul className="hidden md:flex flex-row flex-1 justify-end items-center gap-8 uppercase text-xs">
+        <ul className="hidden flex-1 flex-row items-center justify-end gap-8 text-xs uppercase md:flex">
           <Link to="/about">
             <li className="btn-navbar">About Us</li>
           </Link>
@@ -26,7 +26,7 @@ function NavbarProp() {
           {/* <li className='btn-navbar sign-in-btn'>Sign In</li> */}
           <GoogleLoginButton />
         </ul>
-        <div className="flex md:hidden flex-1 justify-end">
+        <div className="flex flex-1 justify-end md:hidden">
           <div className="cursor-pointer space-y-2">
             <div className="my-style"></div>
             <div className="my-style"></div>
--- blurp-app/src/pages/contact.jsx
+++ blurp-app/src/pages/contact.jsx
@@ -1,11 +1,77 @@
-//Fun Spline3D page
-const ContactPage = () => {
-    return (
-        <>
-        <div>CONTACT PAGE</div>
-        <div>Add contact form, report bug, send email form, etc.</div>
-        </>
-    )
-}
+import FooterProp from '../components/footer';
+import NavbarProp from '../components/navbar';
+import PSU_IMG from '../assets/PSU.jpg';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
 
-export default ContactPage; 
+/* 
+Fixed background following this stackoverflow post
+https://stackoverflow.com/questions/66025707/how-do-you-set-a-full-page-background-color-in-tailwind-css
+*/
+const ContactPage = () => {
+  return (
+    <div className="landing-background min-w-screen min-h-screen">
+      <NavbarProp />
+      <div className="m-4 md:grid md:grid-cols-6 lg:m-8 lg:grid-cols-8">
+        <div className="col-span-3">
+          <h1 className="contact-page-label">Let us know your thoughts!</h1>
+          <p className="text-2xl text-neutral-300">
+            Want to make a comment about our app? Or found a bug while using our app? Let us know by
+            filling out the form below!
+          </p>
+          <form className="my-4 md:grid md:grid-cols-2">
+            <label className="contact-page-label" htmlFor="sender-name">
+              Name
+            </label>
+            <input type="text" id="sender-name" placeholder="Name" className="contact-page-text" />
+            <label className="contact-page-label" htmlFor="sender-email">
+              Email
+            </label>
+            <input
+              type="text"
+              placeholder="Email"
+              className="contact-page-text"
+              id="sender-email"
+            />
+            <label className="contact-page-label" htmlFor="sender-msg">
+              Message
+            </label>
+            <textarea
+              className="contact-page-text col-span-2"
+              placeholder="Write your thoughts here"
+              id="sender-msg"
+              rows="10"
+            />
+            <button className="contact-page-button">Submit</button>
+          </form>
+        </div>
+        <div className="md:col-span-1 lg:col-span-3" />
+        <div className="col-span-2">
+          <img
+            className="scale-95 rounded-lg"
+            src={PSU_IMG}
+            alt="An image of the PSU park blocks"></img>
+          <div className="mx-4 text-right text-2xl text-neutral-300">
+            <p>
+              Portland State University
+              <br />
+              1825 SW Broadway
+              <br />
+              Portland, OR 97201
+            </p>
+            <br />
+            <a href="mailto:blurp.mgmt@gmail.com" className="mx-4">
+              <EmailIcon className="scale-[2.0]" />
+            </a>
+            <a href="https://github.com/pdx-blurp" className="mx-4">
+              <GitHubIcon className="scale-[2.0]" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <FooterProp />
+    </div>
+  );
+};
+
+export default ContactPage;
